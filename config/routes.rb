455b997f1b0e9ed94d7391451b8b88dd82935806
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-07 08:28:56 +0000

    [Add]tea_typeのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'tea_types/index'
-    get 'tea_types/edit'
-  end
 # ルートパス
 root :to =>"public/homes#top"
 
-# ユーザー用
+# ユーザー用devise
 # URL /users/sign_in ...
 devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
+# 管理者用devise
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
 
+
+# 管理者用
+# URL /admin/...
+namespace :admin do
+  resources :tea_types, only:[:index, :create, :edit, :update, :destroy]
+end
+
+# ユーザー用
+# URL /users/...
 scope module: :public do
   resources :users, only:[:index, :show, :edit, :update]
 end
