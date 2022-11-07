91db198bba7aec99ec60a9df8959f53630f61d2c
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-07 08:37:35 +0000

    [Add]tea_typesコントローラ追記

--- app/controllers/admin/tea_types_controller.rb
+++ app/controllers/admin/tea_types_controller.rb
@@ -1,7 +1,31 @@
 class Admin::TeaTypesController < ApplicationController
+
+  def create
+    @tea_type = TeaType.new
+    @tea_type.save
+    redirect_to request.referer
+
+  end
+
   def index
+    @tea_types = TeaType.all
   end
 
   def edit
+    @tea_type = TeaType.find(params[:id])
+
   end
+
+  def update
+    @tea_type = TeaType.find(params[:id])
+    @tea_type = TeaType.update
+    redirect_to admin_tea_types_path
+  end
+
+  def destroy
+    @tea_type = TeaType.find(params[:id])
+    @tea_type.destroy
+    redirect_to request.referer
+  end
+
 end
