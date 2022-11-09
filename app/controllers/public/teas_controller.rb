fe1646bc28ef8513ee4984c5640d7f8e9c87cbff
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-09 12:19:57 +0000

    [Add]タグ検索機能実装

--- app/controllers/public/teas_controller.rb
+++ app/controllers/public/teas_controller.rb
@@ -52,6 +52,15 @@ class Public::TeasController < ApplicationController
     @tea.destroy
     redirect_to user_path(@tea.user)
   end
+  
+  def search_tag
+    #検索結果画面でもタグ一覧表示
+    @tag_list = Tag.all
+    #検索されたタグを受け取る 
+    @tag = Tag.find(params[:tag_id])
+    #検索されたタグに紐づく投稿を表示
+    @teas = @tag.teas
+  end
 
   # def ranking
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 # ルートパス
 root :to =>"public/homes#top"
 
+
 # ユーザー用devise
 # URL /users/sign_in ...
 devise_for :users, skip: [:passwords], controllers: {
@@ -27,9 +28,15 @@ end
 # URL /users/...
 scope module: :public do
   resources :users, only:[:index, :show, :edit, :update]
-  resources :teas, only:[:show, :update, :edit, :destroy, :new, :create]
+  resources :teas, only:[:show, :update, :edit, :destroy, :new, :create] 
+  # タグ検索
+  #タグによって絞り込んだ投稿を表示するアクションへのルーティング
+  resources :tags, only:[:search_tag] do
+    get 'teas', to: 'teas#search_tag'
+  end
+  
 end
 
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
