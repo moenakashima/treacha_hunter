490e02e2e7bb1498997c6eaf5e51947de6256048
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-10 13:12:00 +0000

    [Add]コメントテーブル作成

--- db/migrate/20221110130459_create_tea_comments.rb
+++ db/migrate/20221110130459_create_tea_comments.rb
@@ -1,7 +1,9 @@
 class CreateTeaComments < ActiveRecord::Migration[6.1]
   def change
     create_table :tea_comments do |t|
-
+      t.integer :tea_id, null: false
+      t.integer :user_id, null: false
+      t.string :comment, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_082049) do
+ActiveRecord::Schema.define(version: 2022_11_10_130459) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,14 @@ ActiveRecord::Schema.define(version: 2022_11_10_082049) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tea_comments", force: :cascade do |t|
+    t.integer "tea_id", null: false
+    t.integer "user_id", null: false
+    t.string "comment", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
