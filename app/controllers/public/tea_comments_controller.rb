7149a1125ebb9d4e4ad014a6245df98cfb6dc649
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-10 14:29:40 +0000

    [Add]コメントコントローラ実装

--- app/controllers/public/tea_comments_controller.rb
+++ app/controllers/public/tea_comments_controller.rb
@@ -1,2 +1,22 @@
 class Public::TeaCommentsController < ApplicationController
+  def create
+    @tea = Tea.find(params[:tea_id])
+    @tea_comment = current_user.tea_comments.new(comment_params)
+    @tea_comment.tea_id = @tea.id
+    @tea_comment.save
+  
+  end
+
+  def destroy
+    @tea = Tea.find(params[:tea_id])
+    @tea_comment = TeaComment.find(params[:id])
+    @tea_comment.destroy
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:tea_comment).permit(:comment)
+  end
 end
