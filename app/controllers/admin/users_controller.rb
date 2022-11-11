8da1db8d1bafbf053a97b5b04b0b0e9675559950
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-11 07:15:40 +0000

    [Add]管理者側ユーザーコントローラ作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,22 @@
+class Admin::UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render :edit
+  
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:prefecture_id, :is_active)
+  end
+end
