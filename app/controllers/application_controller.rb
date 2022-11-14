d51b80edb210a0f6c5faf8d3dbc48f94354f3df0
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-14 06:37:46 +0000

    [Add]ヘッダーカラー変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-  new_user_session_path
+  about_path
   end
 
   def after_sign_up_path_for(resource)
