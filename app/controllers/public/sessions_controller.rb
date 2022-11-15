784e84dc7757f312aac9cace0e14a00798a30b62
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-15 07:44:58 +0000

    [Add]停止中アカウントのログイン制御機能追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :user_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,7 +18,21 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 会員ステータスを判断するメソッド
+    def user_state
+      ## 【処理内容1】 入力されたemailからアカウントを1件取得
+      @user = User.find_by(email: params[:user][:email])
+      ## アカウントを取得できなかった場合、このメソッドを終了する
+      return if !@user
+      if @user
+      ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+        if @user.valid_password?(params[:user][:password]) && (@user.is_active == false)
+          ## 【処理内容3】
+          redirect_to user_session_path, notice: "このアカウントは管理者により利用が一時停止されております。"
+        end
+      end
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
