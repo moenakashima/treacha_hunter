c8b34ae7b0e79fb15a9847c6d3c40f80f68aeeed
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-15 07:30:26 +0000

    [Add]アバウトページバリデーション解除

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:about]
   
   def top
     @teas = Tea.all
