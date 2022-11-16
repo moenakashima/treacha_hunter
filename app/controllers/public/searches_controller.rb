456f9c50c2787cdec76b1f323de5137072e3347c
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-16 03:38:10 +0000

    [Add]ページネーション追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,9 @@
 class Public::SearchesController < ApplicationController
   def search
     if (params[:keyword])[0] == '#'
-      @teas = Tag.search(params[:keyword])&.order('created_at DESC')
+      @teas = Tag.search(params[:keyword])&.order('created_at DESC').page(params[:page])
     else
-      @teas = Tea.search(params[:keyword])&.order('created_at DESC')
+      @teas = Tea.search(params[:keyword])&.order('created_at DESC').page(params[:page])
     end
   end
 
