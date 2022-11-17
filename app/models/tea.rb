2cdf819de0080d76e42d9722dc5d1b99cb819237
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-17 05:47:01 +0000

    [Add]デフォルト画像削除

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -18,17 +18,14 @@ class Tea < ApplicationRecord
   
   has_one_attached :tea_image
   
-  # 感想、商品名、お茶の種類にバリデーションを設定
+  # 投稿画像、感想、商品名、お茶の種類にバリデーションを設定
   validates :opinion, length: { maximum: 500 }
   validates :product_name, presence: true
+  validates :tea_image, presence: true
   validates :tea_type, presence: true
   
-  # 投稿時に画像が設定されていない場合にデフォルト画像を表示させる
+  # 画像のサイズ指定の可変化設定
   def get_tea_image(width, height)
-    unless tea_image.attached?
-    file_path = Rails.root.join('app/assets/images/default-image.jpg')
-    tea_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
-    end
     tea_image.variant(resize_to_limit: [width, height]).processed
   end
   
