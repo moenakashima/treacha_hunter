cb3200f69d5ae4c783b6ecf758803891e6a089b8
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-17 03:50:22 +0000

    [Add]絞り込み機能修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,9 @@
 class Public::SearchesController < ApplicationController
   def search
     if (params[:keyword])[0] == '#'
-      @teas = Tag.search(params[:keyword])&.order('created_at DESC').page(params[:page])
+      @teas = Tag.search(params[:keyword])&.order('created_at DESC')&.page(params[:page])
     else
-      @teas = Tea.search(params[:keyword])&.order('created_at DESC').page(params[:page])
+      @teas = Tea.search(params[:keyword])&.order('created_at DESC')&.page(params[:page])
     end
   end
 
