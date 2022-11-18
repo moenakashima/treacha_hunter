fa032e14b81c5f736ea7f587307c1bda598c237c
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-18 06:35:43 +0000

    [Add]jsを読み込めるように変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # 追記
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
 end
