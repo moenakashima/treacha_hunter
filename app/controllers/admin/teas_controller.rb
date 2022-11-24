8ccc89929c517945933c6800a771c3613f5a7326
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-24 02:00:24 +0000

    [Add]showアクション編集

--- app/controllers/admin/teas_controller.rb
+++ app/controllers/admin/teas_controller.rb
@@ -9,7 +9,10 @@ class Admin::TeasController < ApplicationController
     @tea = Tea.find(params[:id])
     @tea_tags = @tea.tags
     @user = @tea.user
-    @tea_comment = TeaComment.new
-    @tea_comments = TeaComment.all.order('created_at DESC')
+    # 参加して何日経ったのかを’ハンター歴’として表示するための日付計算       
+    @d1 = Date.today
+    @d2 = (@user.created_at).to_date
+    # 今日の日付と参加日の差分を計算し文字列で取得    
+    @hunter_histroy = (@d1 - @d2).to_i
   end
 end
