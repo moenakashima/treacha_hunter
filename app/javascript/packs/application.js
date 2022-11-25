cfc3ac8201765c17f3aaef30f096d3957d55e60a
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-25 11:00:35 +0000

    [Add]Turbolinks無効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+// ターボリンク無効化
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -12,3 +14,5 @@ import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 ActiveStorage.start()
+// ターボリンク無効化
+// Turbolinks.start() 
