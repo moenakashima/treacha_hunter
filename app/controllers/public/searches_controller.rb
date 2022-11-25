fcd5cceb002ca181bbf684d771d6d2734f168b79
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-25 10:57:23 +0000

    [Add]インデント追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,10 +1,14 @@
 class Public::SearchesController < ApplicationController
+  
   def search
+    #『#』が先頭に着いたら、タグで検索をする
     if (params[:keyword])[0] == '#'
       @teas = Tag.search(params[:keyword])&.order('teas.created_at DESC')&.page(params[:page])
       render "public/homes/top"
+    # 未入力で検索したら、トップページをレンダー
     elsif params[:keyword] == ""
       render "public/homes/top"
+    # それ以外の検索は投稿内容で検索
     else
       @teas = Tea.search(params[:keyword])&.order('teas.created_at DESC')&.page(params[:page])
       render "public/homes/top"
