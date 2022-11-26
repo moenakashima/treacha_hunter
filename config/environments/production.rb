6b63199acbbcdc2fd45b9d29c1ba22feed257cce
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-26 05:55:31 +0000

    [Fix]Uglifierのコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   # 追記
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # rails5系の使用では動くが6
+  #config.assets.js_compressor = Uglifier.new(harmony: true)
 
 end
