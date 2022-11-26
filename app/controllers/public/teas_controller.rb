1712ec04fde9cb54b460b925a6e7311fcb45839d
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-26 03:22:12 +0000

    [Add]ゲストユーザーの閲覧／編集権限を設定

--- app/controllers/public/teas_controller.rb
+++ app/controllers/public/teas_controller.rb
@@ -1,5 +1,7 @@
 class Public::TeasController < ApplicationController
   
+  before_action :ensure_correct_user, only: [:update]
+  
   def create
     @tea = Tea.new(tea_params)
     @tea.user_id = current_user.id
@@ -17,6 +19,9 @@ class Public::TeasController < ApplicationController
 
   def new
     @tea = Tea.new
+    if current_user.name == "guestuser"
+      redirect_to root_path, notice: 'ゲストユーザーは閲覧のみ可能です' 
+    end
   end
 
   def edit
@@ -88,5 +93,11 @@ class Public::TeasController < ApplicationController
     params.require(:tea).permit(:product_name, :prefecture_id, :tea_image, :seller, :tea_type_id, :parchased_at, :opinion)
   end
   
-
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
