478894e25a369a814cbc617254977a4df2c1dfb8
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-26 02:31:19 +0000

    [Add]再デプロイ時のNameError: uninitialized constant Uglifierエラー解消のため

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require_relative "boot"
 
 require "rails/all"
 
+# 追記(再deploy時のNameError: uninitialized constant Uglifierのため）
+require 'uglifier'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
