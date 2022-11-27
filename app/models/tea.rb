8df63b68e864fbf0d455ed694d9ab83a62e803cc
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-27 08:48:51 +0000

    [Fix]全角スペース削除

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -53,7 +53,7 @@ class Tea < ApplicationRecord
   
     # 古いタグを消す
     old_tags.each do |old|
-      self.tags.delete　Tag.find_by(name: old)
+      self.tags.delete Tag.find_by(name: old)
     end
   
     # 新しいタグを保存
