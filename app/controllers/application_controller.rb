92a70b0f1a29ac384741ddc27b7e405ddca97b4c
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-11-27 06:59:20 +0000

    [fix]不要コード削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,14 @@
 class ApplicationController < ActionController::Base
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
 
   def after_sign_in_path_for(resource)
-  root_path
+  top_path
   end
 
-  def after_sign_out_path_for(resource)
-  about_path
-  end
-
-  def after_sign_up_path_for(resource)
-  root_path
-  end
 
 
   def configure_permitted_parameters
