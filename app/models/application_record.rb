933894c1ea952338fc951ad06e1f7ec4845e1caf
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-12-17 15:40:27 +0000

    [Add]updateアクションのcustom validationメソッド作成

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  
+  def update_with_context(new_attributes, context)
+    with_transaction_returning_status do
+      assign_attributes(new_attributes)
+      save(context: context)
+    end
+  end
 end
